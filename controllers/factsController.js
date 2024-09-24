8636c4ccddb3087469d727cb821354dab4c7365b
Jideotetic <jideotetic@gmail.com>
2024-09-24 15:06:54 +0100

    Added error handling✔

--- controllers/factsController.js
+++ controllers/factsController.js
@@ -1,5 +1,6 @@
 const { v4: uuidv4 } = require("uuid");
 const { format } = require("date-fns");
+const asyncHandler = require("express-async-handler");
 
 const randomFacts = [
 	{
@@ -22,15 +23,15 @@ const randomFacts = [
 	},
 ];
 
-const getRandomFacts = (req, res) => {
+const getRandomFacts = asyncHandler((req, res) => {
 	res.render("index", { title: "Random Cool Facts", randomFacts: randomFacts });
-};
+});
 
-const createNewFact = (req, res) => {
+const createNewFact = asyncHandler((req, res) => {
 	res.render("form", { title: "Random Cool Facts" });
-};
+});
 
-const addNewFact = (req, res) => {
+const addNewFact = asyncHandler((req, res) => {
 	randomFacts.push({
 		id: uuidv4(),
 		text: req.body.newFact,
@@ -38,11 +39,15 @@ const addNewFact = (req, res) => {
 		added: format(new Date(), "yyyy-MM-dd"),
 	});
 	res.redirect("/");
-};
+});
 
-const getFactDetails = (req, res) => {
+const getFactDetails = asyncHandler((req, res) => {
 	const fact = randomFacts.find((fact) => fact.id === req.params.factDetail);
+	if (!fact) {
+		res.status(404).send("Page not found");
+		return;
+	}
 	res.render("fact-details", { title: "Random Cool Facts", fact: fact });
-};
+});
 
 module.exports = { getRandomFacts, addNewFact, createNewFact, getFactDetails };
