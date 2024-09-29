ad9f260cc71b53c4c52ab1a9b539e98df9a12761
Jideotetic <jideotetic@gmail.com>
2024-09-29 22:56:32 +0100

    Setup db

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	database: process.env.DB_NAME,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PROT,
-});
-
 // module.exports = new Pool({
-// 	connectionString: process.env.DB_URL,
+// 	host: process.env.DB_HOST,
+// 	user: process.env.DB_USER,
+// 	database: process.env.DB_NAME,
+// 	password: process.env.DB_PASSWORD,
+// 	port: process.env.DB_PROT,
+// 	ssl: { rejectUnauthorized: false },
 // });
+
+module.exports = new Pool({
+	connectionString: process.env.DB_URL,
+});
